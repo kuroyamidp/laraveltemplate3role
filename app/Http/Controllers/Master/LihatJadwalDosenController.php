938b55e13f4fb87a67b180c5274333ce717bdf30
fah rizal <2215102008@ittelkom-pwt.ac.id>
2023-03-15 09:52:04 +0700

    harini

---
+++ app/Http/Controllers/Master/LihatJadwalDosenController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\master;
+
+use App\Http\Controllers\Controller;
+use App\Imports\DataMatkulImport;
+use App\Models\Master\MatakuliahModel;
+use Illuminate\Http\Request;
+use App\Models\Master\MahasiswaModel;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Maatwebsite\Excel\Facades\Excel;
+use PDF;
+
+class LihatJadwalUjianDosenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        {
+            $data['matkul'] = MatakuliahModel::get();
+            return view('pages.lihatjadwaldsn.lihatjadwaldsn', $data);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/pages/lihatjadwaldsn/lihtajadwal.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="layout-px-spacing">
+
+    <div class="row layout-top-spacing">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <a href="{{route('jadwalujian.create')}}" class="btn btn-primary btn-sm">Print</a>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <table class="table table-hover" id="default-ordering">
+                                <thead>
+                                    <tr class="text-center">
+                                        <th>No</th>
+                                        <th>Mata kuliah</th>
+                                        <th>Ruang</th>
+                                        <th>Dosen</th>
+                                        <th>Jam</th>
+                                        <th>Tanggal Ujian</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $no=1
+                                    @endphp
+                                    @foreach ($jadwalujians as $v)
+                                    <tr style="text-align: center;">
+                                        <td width="1%">{{$no++}}</td>
+                                        <td>{{$v->matkul}}</td>
+                                        <td>{{$v->ruang}}</td>
+                                        <td>{{$v->dosen}}</td>
+                                        <td>{{$v->jam}}</td>
+                                        <td>{{$v->tanggal}}</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
